c715ca03e82cfdde732f7309e7ec0f811d9d4268
YutLee <664009360@qq.com>
2022-01-28 12:02:45 +0800

    修复useScroll未导出的问题

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import useMount from './use-mount'
 import useUnmount from './use-unmount'
 import useMountEffect from './use-mount-effect'
 import useFetch from './use-fetch'
+import useScroll from './use-scroll'
 
 export {
   useMount,
   useUnmount,
   useMountEffect,
-  useFetch
+  useFetch,
+  useScroll
 }
